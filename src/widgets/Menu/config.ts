a1fd1dad0224d1a42ae90e556b71fdd24e014764
amp <ampanahi20@gmail.com>
2021-06-10 17:24:43 +0430

    first commit

---
+++ src/widgets/Menu/config.ts
@@ -0,0 +1,186 @@
+export const links = [
+  {
+    label: "Home",
+    icon: "HomeIcon",
+    href: "/",
+  },
+  {
+    label: "Trade",
+    icon: "TradeIcon",
+    items: [
+      {
+        label: "Exchange",
+        href: "https://swap.fenixfinance.net",
+      },
+      {
+        label: "Liquidity",
+        href: "https://swap.fenixfinance.net/#/pool",
+      },
+    ],
+  },
+  {
+    label: "Farms",
+    icon: "FarmIcon",
+    href: "/farms",
+  },
+  {
+    label: "Pools",
+    icon: "PoolIcon",
+    href: "/syrup",
+  },
+  {
+    label: "Lottery",
+    icon: "TicketIcon",
+    href: "/lottery",
+  },
+  {
+    label: "NFT",
+    icon: "NftIcon",
+    href: "/nft",
+  },
+  {
+    label: "Profile & Teams",
+    icon: "GroupsIcon",
+    items: [
+      {
+        label: "Leaderboard",
+        href: "/",
+      },
+      {
+        label: "YourProfile",
+        href: "/",
+      },
+    ],
+    calloutClass: "rainbow",
+  },
+  {
+    label: "Info",
+    icon: "InfoIcon",
+    items: [
+      {
+        label: "Overview",
+        href: "https://pancakeswap.info",
+      },
+      {
+        label: "Tokens",
+        href: "https://pancakeswap.info/tokens",
+      },
+      {
+        label: "Pairs",
+        href: "https://pancakeswap.info/pairs",
+      },
+      {
+        label: "Accounts",
+        href: "https://pancakeswap.info/accounts",
+      },
+    ],
+  },
+  {
+    label: "IFO",
+    icon: "IfoIcon",
+    items: [
+      {
+        label: "Next",
+        href: "/ifo",
+      },
+      {
+        label: "History",
+        href: "/ifo/history",
+      },
+    ],
+  },
+  {
+    label: "More",
+    icon: "MoreIcon",
+    items: [
+      // {
+      //   label: "Voting",
+      //   href: "https://voting.fenixfinance.net",
+      // },
+      {
+        label: "Github",
+        href: "https://github.com/fenixfinance/",
+      },
+      {
+        label: "Blog",
+        href: "https://medium.com/@fenixfinanceofficial/",
+      },
+    ],
+  },
+];
+
+export const socials = [
+  {
+       label: "Telegram",
+    icon: "TelegramIcon",
+    items: [
+      {
+        label: "English",
+        href: "https://t.me/FenixFinanceChat",
+      },
+      // {
+      //   label: "Bahasa Indonesia",
+      //   href: "https://t.me/fenixswapIndonesia",
+      // },
+      {
+        label: "中文",
+        href: "https://t.me/fenixswapchinese",
+      },
+      // {
+      //   label: "Tiếng Việt",
+      //   href: "https://t.me/fenixswapVN",
+      // },
+      {
+        label: "Italiano",
+        href: "https://t.me/fenixswapitalian",
+      },
+      {
+        label: "русский",
+        href: "https://t.me/fenixswaprussian",
+      },
+      {
+        label: "Türkiye",
+        href: "https://t.me/fenixswapTurkey",
+      },
+      // {
+      //   label: "Português",
+      //   href: "https://t.me/fenixswapPortuguese",
+      // },
+      {
+        label: "Español",
+        href: "https://t.me/fenixswapspanish",
+      },
+      {
+        label: "日本語",
+        href: "https://t.me/fenixswapjapanese",
+      },
+      {
+        label: "Français",
+        href: "https://t.me/fenixswapfrench",
+      },
+      {
+        label: "Announcements",
+        href: "https://t.me/fenixfinance",
+      },
+      {
+        label: "Price Bot",
+        href: "https://t.me/BinanceRocketFXF",
+      },
+    ],
+  },
+  {
+    label: "Twitter",
+    icon: "TwitterIcon",
+    href: "https://twitter.com/fenixfinance",
+  },
+  {
+    label: "Github",
+    icon: "GithubIcon",
+    href: "https://www.Github.com/fenixfinance",
+  },
+];
+
+export const MENU_HEIGHT = 64;
+export const MENU_ENTRY_HEIGHT = 48;
+export const SIDEBAR_WIDTH_FULL = 240;
+export const SIDEBAR_WIDTH_REDUCED = 56;
